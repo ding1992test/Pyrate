e10a6fad66a6d75793e4a59a4fdcc58ef6000c86
ding1992test <42364424+ding1992test@users.noreply.github.com>
2018-08-30 14:21:00 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+package_name = "Pyrate"
+package_version = "1.0.0"
+
+setup(
+    name=package_name,
+    version=package_version,
+    author="rojan-rijal"
+)
